9b84fec670a7f670d76089bfc68c1eb54d7c39e6
St <stellawang2021@gmail.com>
2021-02-12 00:25:58 -0500

    first commit

---
+++ MovieShop.MVC/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieShop.MVC/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //https://localhost:44363/Movies/Index
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            //By defaul when u have return view()
+            // u can change that
+            //return View("Testing");
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopRevenueMovies()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopRatedMovies()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Genre(int genreId)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Reviews(int movieId)
+        {
+            return View();
+        }
+    }
+}
